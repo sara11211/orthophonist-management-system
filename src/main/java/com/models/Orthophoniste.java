4fec39c6546fa16fad032fa12b993bf08d7f8910
saraiwr <117382906+sara11211@users.noreply.github.com>
2024-05-11 01:41:07 +0200

    orthophonist class added

--- src/main/java/com/models/Orthophoniste.java
+++ src/main/java/com/models/Orthophoniste.java
@@ -1,7 +1,12 @@
 package com.models;
 
-public class Orthophoniste {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public class Orthophoniste implements Serializable {
+
+    // attributs
     private String nom;
     private String prenom;
     private String adresse;
@@ -9,39 +14,26 @@ public class Orthophoniste {
     private String adresseEmail;
     private String motDePasse;
 
+    // constructeurs
     public Orthophoniste(String nom, String prenom, String adresse, String numTel, String adresseEmail, String motDePasse) {
         this.nom = nom;
         this.prenom = prenom;
         this.adresse = adresse;
         this.numTel = numTel;
         this.adresseEmail = adresseEmail;
-        this.motDePasse = motDePasse
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public void setAdresse(String adresse) {
-        this.adresse = adresse;
-    }
-    
-    public void setNumTel(String numTel) {
-        this.numTel = numTel;
-    }
-
-    public void setAdresseEmail(String adresseEmail) {
-        this.adresseEmail = adresseEmail;
-    }
-
-    public void setMotDePasse(String motDePasse) {
         this.motDePasse = motDePasse;
     }
 
+    public Orthophoniste(String adresseEmail, String motDePasse) {
+        this.adresseEmail = adresseEmail;
+        this.motDePasse = motDePasse;
+    }
+
+    public Orthophoniste(String adresseEmail) {
+        this.adresseEmail = adresseEmail;
+    }
+
+    // getters & setters
     public String getNom() {
         return nom;
     }
@@ -61,8 +53,49 @@ public class Orthophoniste {
     public String getAdresseEmail() {
         return adresseEmail;
     }
-
+    
     public String getMotDePasse() {
         return motDePasse;
     }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setNumTel(String numTel) {
+        this.numTel = numTel;
+    }
+
+    public void setAdresseEmail(String adresseEmail) {
+        this.adresseEmail = adresseEmail;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    // affichage
+    public String toString() {
+        return "Email : " + this.adresseEmail + ", Mot de passe : " + this.motDePasse;
+    }
+
+    // redifinitions 
+    @Override
+    public int hashCode() {
+        return getAdresseEmail().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.pseudo.equals(((Orthophoniste) obj).getAdresseEmail());
+    }
+    
 }
