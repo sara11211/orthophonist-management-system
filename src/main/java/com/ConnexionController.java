0c886ffbc35ba7cdaf90d8d2b8655eecd226dfbc
saraiwr <117382906+sara11211@users.noreply.github.com>
2024-05-14 13:06:23 +0200

    inscription added (temperory ui to try)

--- src/main/java/com/ConnexionController.java
+++ src/main/java/com/ConnexionController.java
@@ -4,9 +4,14 @@ import com.models.UtilisateurNonTrouveException;
 import com.models.Orthophoniste;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -21,6 +26,27 @@ public class ConnexionController {
         private TextField adresseEmail;
         @FXML
         private PasswordField motDePasse;
+
+        private Stage stage;
+        private Scene scene;
+        private Parent root;
+
+        @FXML 
+        void signupButton(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signup.fxml"));
+        scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't load FXML file");
+        }
+
+        Button button = (Button) event.getSource();
+        stage = (Stage) button.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+        }
     
         @FXML
         void seConnecterButton(ActionEvent event) throws UtilisateurNonTrouveException, IOException {
@@ -46,4 +72,5 @@ public class ConnexionController {
             }
         }
     }
+
 
---
+++ src/main/java/com/InscriptionController.java
@@ -0,0 +1,71 @@
+package com;
+
+import com.models.OMS;
+import com.models.Orthophoniste;
+import javafx.css.PseudoClass;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+
+public class InscriptionController {
+
+    @FXML
+    private Label erreurText;
+    @FXML
+    private TextField email;
+    @FXML
+    private PasswordField motDePasse;
+
+    @FXML
+    void seConnecterButton(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = null;
+        try
+        {
+            scene = new Scene(fxmlLoader.load());
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            System.out.println("Couldn't load FXML file");
+        }
+
+        Button button = (Button)event.getSource();
+        Stage stage = (Stage)button.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void inscriptionButton(ActionEvent event) {
+
+        String userEmail = email.getText();
+        String userMotDePasse = motDePasse.getText();
+
+        if (HelloApplication.oms.isExist(userEmail, userMotDePasse))
+        {
+            System.out.println("Pseudo existe deja!");
+        }
+        else
+        {
+            HelloApplication.utilisateurCourant = new Orthophoniste(userEmail,userMotDePasse);
+            HelloApplication.oms.getOrthophonistes().put(HelloApplication.utilisateurCourant,userMotDePasse);
+            System.out.println("ajouter user : "+userEmail+"  mdp : "+userMotDePasse);
+        }
+    }
+}
+
